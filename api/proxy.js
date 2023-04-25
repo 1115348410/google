e57c218622f8d70c7e4f358a62e5ad6f00ea1cb9
yuwei <yuwei@andunip.com>
2023-04-25 10:11:58 +0800

    edg

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
 
-  let target = "https://chat.openai.com/";
+  let target = "https://bard.google.com/";
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
